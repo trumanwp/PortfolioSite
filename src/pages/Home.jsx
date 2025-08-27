32192adf92feba43edc5218600fc53f85510992e
trumanwp <adambowieday3@gmail.com>
2025-08-27 16:20:21 +0100

    Added HeroSection.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
@@ -15,6 +16,9 @@ export const Home = () => {
       <Navbar />
 
       {/* Main Content */}
+      <main>
+        <HeroSection />
+      </main>
 
       {/* Footer */}
     </div>
