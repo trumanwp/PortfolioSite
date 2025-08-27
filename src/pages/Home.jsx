3897e18eb9d3bf2455a418060c581ea41654b8b1
trumanwp <adambowieday3@gmail.com>
2025-08-27 14:24:39 +0100

    Added stylings to index.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
 export const Home = () => {
-    return <div>Home</div>
+    return <div className="min-h-screen"> 
+
+        {/* {Theme Toggle} */}
+
+        {/* {Background Effects} */}
+
+        {/* {Navbar} */}
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+    </div>
+    
 }
