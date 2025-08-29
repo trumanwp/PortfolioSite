9cc4eca7fb52e5b0ecf3126b64f7748234bc1ea5
trumanwp <adambowieday3@gmail.com>
2025-08-29 22:24:49 +0100

    Modified cv file

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public\AdamBOWIECS_CV.docx"
+                href="/AdamBOWIECS_CV.docx"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
